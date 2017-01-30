a9b59da22d35698e9141183f82b6a3cbc14dc7f5
Laura Abend <laura.l.abend@gmail.com>
2017-01-30 17:41:09 -0600

    add build.sh for vendors directory

---
+++ build.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+mkdir -p public/vendors;
+
+# angular
+cp node_modules/angular/angular.min.js public/vendors;
+cp node_modules/angular/angular.min.js.map public/vendors;
+
+# angular-route
+cp node_modules/angular-route/angular-route.min.js public/vendors;
+cp node_modules/angular-route/angular-route.min.js.map public/vendors;
+
+# bootstrap
+cp node_modules/bootstrap/dist public/vendors;
